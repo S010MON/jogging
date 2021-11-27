4db1e42913bf86f860b968c698962711312630ae
S010MON <10490509+S010MON@users.noreply.github.com>
2021-11-27 16:28:15 +0100

    simplifies logging to single input and default path values

--- src/main/java/Logger.java
+++ src/main/java/Logger.java
@@ -4,70 +4,50 @@ import java.io.IOException;
 
 public class Logger
 {
-    private String filePath;
+    private String directoryPath;
+    private String fileName;
 
     /**
      * Default constructor path set to projectRoot/log/
      */
     public Logger()
     {
-        this.filePath = FilePath.getInternal("/logs/");
-        File dir = new File(filePath);
-        createDirectory(dir);
+        this.directoryPath = FilePath.getInternal("/logs/");
+        this.fileName = "log";
+        createDirectory();
+    }
+
+    /**
+     * Set different logging location using the internal filePath from the project root
+     * @param fileName - "log"
+     */
+    public Logger(String fileName)
+    {
+        this.directoryPath = FilePath.getInternal("/logs/");
+        this.fileName = fileName;
+        createDirectory();
     }
 
     /**
      * Set different logging location using the internal filePath from the project root
      * @param internalFilePath - "/src/main/java/src/config/"
+     * @param fileName - "log"
      */
-    public Logger(String internalFilePath)
+    public Logger(String internalFilePath, String fileName)
     {
-        this.filePath = FilePath.getInternal(internalFilePath);
-        File dir = new File(filePath);
-        createDirectory(dir);
+        this.directoryPath = FilePath.getInternal(internalFilePath);
+        this.fileName = fileName;
+        createDirectory();
     }
 
-    public void createDirectory(File file)
+    public void createDirectory()
     {
+        File file = new File(directoryPath + fileName);
         if(!file.exists() || !file.isDirectory())
         {
             boolean success = file.mkdirs();
             if (!success)
                 throw new RuntimeException("Logging directory was not created");
-        }
-    }
-
-    /**
-     * Append a string to a new line of a {@code .txt} file
-     * Creates a new file if one cannot be found
-     * @param fileName  - The name of the file
-     * @param str		- The string to append
-     */
-    public void log(String fileName, String str)
-    {
-        try
-        {
-            fileName = fileName.concat(".txt");
-            String filePath = this.filePath + fileName;
-            File file = new File(filePath);
-
-            if(!file.exists())
-            {
-                boolean success = file.createNewFile();
-                if (!success)
-                    throw new RuntimeException("File: " + fileName + " was not created");
-            }
-
-            FileWriter writer = new FileWriter(file, true);
-            writer.write(str);
-            writer.append("\n");
-            writer.close();
-        }
-        catch(IOException e)
-        {
-            System.out.println("File not found");
-            System.out.println("File path used: " + this.filePath + fileName);
-            e.printStackTrace();
         }
     }
 
@@ -80,26 +60,15 @@ public class Logger
     {
         try
         {
-            String fileName = "log.txt";
-            String filePath = this.filePath + fileName;
-            File file = new File(filePath);
-
-            if(!file.exists())
-            {
-                boolean success = file.createNewFile();
-                if (!success)
-                    throw new RuntimeException("File: " + fileName + " was not created");
-            }
-
+            File file = getTxtFile();
             FileWriter writer = new FileWriter(file, true);
             writer.write(str);
             writer.append("\n");
             writer.close();
-        }
-        catch(IOException e)
-        {
-            System.out.println("File not found");
-            System.out.println("File path used: " + this.filePath + "log.txt");
+        } catch (IOException e) {
+            System.out.println("unable to log to csv" +
+                    "\nPath: " + directoryPath +
+                    "\nName: " + fileName);
             e.printStackTrace();
         }
     }
@@ -107,33 +76,20 @@ public class Logger
     /**
      * Append a string to a new line of a {@code .csv} file
      * Creates a new file if one cannot be found
-     * @param fileName  - The name of the file
      * @param str		- The comma delimited string to append
      */
-    public void logCSV(String fileName, String str)
+    public void logCSV(String str)
     {
-        try
-        {
-            fileName = fileName.concat(".csv");
-            String filePath = this.filePath + fileName;
-            File file = new File(filePath);
-
-            if(!file.exists())
-            {
-                boolean success = file.createNewFile();
-                if (!success)
-                    throw new RuntimeException("File: " + fileName + " was not created");
-            }
-
+        try {
+            File file = getCsvFile();
             FileWriter writer = new FileWriter(file, true);
             writer.write(str);
             writer.append("\n");
             writer.close();
-        }
-        catch(IOException e)
-        {
-            System.out.println("File not found");
-            System.out.println("File path used: " + this.filePath + fileName);
+            } catch (IOException e) {
+            System.out.println("unable to log to csv" +
+                    "\nPath: " + directoryPath +
+                    "\nName: " + fileName);
             e.printStackTrace();
         }
     }
@@ -141,36 +97,53 @@ public class Logger
     /**
      * Append a string to a new line of a {@code .csv} file
      * Creates a new file if one cannot be found
-     * @param fileName  - The name of the file
      * @param str		- The array of strings to append with comma delimiters
      */
-    public void logCSV(String fileName, String[] str)
+    public void logCSV(String[] str)
     {
-        try
-        {
-            fileName = fileName.concat(".csv");
-            String filePath = this.filePath + fileName;
-            File file = new File(filePath);
-
-            if(!file.exists())
-            {
-                boolean success = file.createNewFile();
-                if (!success)
-                    throw new RuntimeException("File: " + fileName + " was not created");
-            }
-
+        try {
+            File file = getCsvFile();
             FileWriter writer = new FileWriter(file, true);
-            for (String s : str)
-            {
+            for (String s : str) {
                 writer.write(s + ",");
             }
             writer.close();
-        }
-        catch(IOException e)
-        {
-            System.out.println("File not found");
-            System.out.println("File path used: " + this.filePath + fileName);
+        } catch (IOException e) {
+            System.out.println("unable to log to csv" +
+                    "\nPath: " + directoryPath +
+                    "\nName: " + fileName);
             e.printStackTrace();
         }
+    }
+
+    private File getTxtFile()
+    {
+        String filePath = directoryPath + fileName + ".txt";
+        return  getFile(filePath);
+    }
+
+    private File getCsvFile()
+    {
+        String filePath = directoryPath + fileName + ".csv";
+        return  getFile(filePath);
+    }
+
+    private File getFile(String filePath)
+    {
+        File file = new File(filePath);
+        try
+        {
+            if (!file.exists()) {
+                boolean success = file.createNewFile();
+                if (!success)
+                    throw new RuntimeException("File: " + fileName + " was not created");
+            }
+
+        }  catch(IOException e) {
+            System.out.println("File not found");
+            System.out.println("File path used: " + filePath);
+            e.printStackTrace();
+        }
+        return file;
     }
 }
