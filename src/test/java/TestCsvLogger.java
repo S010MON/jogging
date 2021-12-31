2bc7ddf563f951230bfb2fc96b4365c59d06c54d
S010MON <leon@bdebnath.com>
2021-12-31 15:21:43 +0000

    refactor tests to improve cleanup and reduce duplication

--- src/test/java/TestCsvLogger.java
+++ src/test/java/TestCsvLogger.java
@@ -1,42 +1,14 @@
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class TestCsvLogger
+public class TestCsvLogger extends TestLogger
 {
-    public static String testPath = "/logs/";
-
-    @BeforeEach void setUp()
-    {
-        File testDir = getFile("");
-        if (!testDir.exists() || !testDir.isDirectory())
-            testDir.mkdir();
-    }
-
-    @AfterEach void tearDown()
-    {
-        File testDir = getFile("");
-        if (testDir.exists())
-        {
-            String[] files = testDir.list();
-            for(String s: files)
-            {
-                File currentFile = new File(testDir.getPath(), s);
-                currentFile.delete();
-            }
-            testDir.delete();
-        }
-    }
-
     @Test void testDefaultLogString()
     {
         String exp = "Hello,darkness,my,old,friend,";
@@ -46,7 +18,7 @@ public class TestCsvLogger
         logger.setToCSV();
         logger.log(exp);
         try {
-            File defaultFile = getFile("log.csv");
+            File defaultFile = getFile(testPaths[0], "log.csv");
             BufferedReader reader = new BufferedReader(new FileReader(defaultFile));
             act = reader.readLine();
         } catch (IOException e) {
@@ -67,7 +39,7 @@ public class TestCsvLogger
         logger.setToCSV();
         logger.log(input);
         try {
-            File defaultFile = getFile("log.csv");
+            File defaultFile = getFile(testPaths[0], "log.csv");
             BufferedReader reader = new BufferedReader(new FileReader(defaultFile));
             act = reader.readLine();
         } catch (IOException e) {
@@ -87,7 +59,7 @@ public class TestCsvLogger
         logger.setToCSV();
         logger.log(exp);
         try {
-            File defaultFile = getFile("log.csv");
+            File defaultFile = getFile(testPaths[0], "log.csv");
             BufferedReader reader = new BufferedReader(new FileReader(defaultFile));
             act = reader.readLine();
         } catch (IOException e) {
@@ -106,7 +78,7 @@ public class TestCsvLogger
         logger.setToCSV();
         logger.log(exp);
         try {
-            File defaultFile = getFile("log.csv");
+            File defaultFile = getFile(testPaths[0],"log.csv");
             BufferedReader reader = new BufferedReader(new FileReader(defaultFile));
             act = reader.readLine();
         } catch (IOException e) {
@@ -126,7 +98,7 @@ public class TestCsvLogger
         logger.setToCSV();
         logger.log(exp);
         try {
-            File defaultFile = getFile("log.csv");
+            File defaultFile = getFile(testPaths[0],"log.csv");
             BufferedReader reader = new BufferedReader(new FileReader(defaultFile));
             act = reader.readLine();
         } catch (IOException e) {
@@ -146,7 +118,7 @@ public class TestCsvLogger
         logger.setToCSV();
         logger.log(exp);
         try {
-            File defaultFile = getFile("log.csv");
+            File defaultFile = getFile(testPaths[0],"log.csv");
             BufferedReader reader = new BufferedReader(new FileReader(defaultFile));
             act = reader.readLine();
         } catch (IOException e) {
@@ -167,7 +139,7 @@ public class TestCsvLogger
         logger.setToCSV();
         logger.log(input);
         try {
-            File defaultFile = getFile("log.csv");
+            File defaultFile = getFile(testPaths[0],"log.csv");
             BufferedReader reader = new BufferedReader(new FileReader(defaultFile));
             act = reader.readLine();
         } catch (IOException e) {
@@ -188,7 +160,7 @@ public class TestCsvLogger
         logger.setToCSV();
         logger.log(input);
         try {
-            File defaultFile = getFile("log.csv");
+            File defaultFile = getFile(testPaths[0],"log.csv");
             BufferedReader reader = new BufferedReader(new FileReader(defaultFile));
             act = reader.readLine();
         } catch (IOException e) {
@@ -210,7 +182,7 @@ public class TestCsvLogger
         logger.setDelimeter("!");
         logger.log(input);
         try {
-            File defaultFile = getFile("log.csv");
+            File defaultFile = getFile(testPaths[0], "log.csv");
             BufferedReader reader = new BufferedReader(new FileReader(defaultFile));
             act = reader.readLine();
         } catch (IOException e) {
@@ -219,13 +191,5 @@ public class TestCsvLogger
         }
 
         assertEquals(exp, act);
-    }
-
-    private File getFile(String name)
-    {
-        FileSystem fileSystem = FileSystems.getDefault();
-        String path = fileSystem.getPath("").toAbsolutePath().toString();
-        path = path.concat(testPath + name);
-        return new File(path);
     }
 }
